1a1b2cf7d036ef5bdcc256f72c33214de7959ec8
Prat <prateekdutt4190@gmail.com>
2022-02-24 11:58:23 +0530

    create registration form with drop down without binding

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using New_RegistrationForm24Feb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace New_RegistrationForm24Feb.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        Registration_FormEntities db = new Registration_FormEntities();
+
+        public ActionResult Index()
+        {
+            List<State> state= db.States.ToList();
+            ViewBag.State1 = new SelectList(state,"State_ID","State_Name");
+
+            List<Country> Countr = db.Countries.ToList();
+            ViewBag.Country1 = new SelectList(Countr, "Country_ID", "Country_Name");
+
+            return View();
+        }
+
+      
+    }
+}
