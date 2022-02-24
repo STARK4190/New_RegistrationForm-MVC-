1cf75690771f4f8fb6b658edea0f3f4c69e1ec44
Prat <prateekdutt4190@gmail.com>
2022-02-24 18:06:40 +0530

    drop down save done in DB(but show error in view drop down code ) ,no dropdown binding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,17 +12,24 @@ namespace New_RegistrationForm24Feb.Controllers
     {
         Registration_FormEntities db = new Registration_FormEntities();
 
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             List<State> state= db.States.ToList();
-            ViewBag.State1 = new SelectList(state,"State_ID","State_Name");
+            ViewBag.State1 = new SelectList(state,"State_ID","State_Name"); //here problem ("as","as")
 
             List<Country> Countr = db.Countries.ToList();
-            ViewBag.Country1 = new SelectList(Countr, "Country_ID", "Country_Name");
+            ViewBag.Country1 = new SelectList(Countr, "Country_ID", "Country_Name"); //here problem ("as","as")
 
             return View();
         }
 
-      
+        [HttpPost]
+        public ActionResult Index(Registration rs) //insert 
+        {
+              db.Registrations.Add(rs);
+              ViewBag.Msg = "<script>alert('done')</script>";
+              return View();
+        }
+
     }
 }
