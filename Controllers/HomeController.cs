6499bf5d6e22bc063ffd280616e5ba186e3dc230
Prat <prateekdutt4190@gmail.com>
2022-02-25 15:41:34 +0530

    save error resolve,delete done ,NOTDONE:only binding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,21 +13,21 @@ namespace New_RegistrationForm24Feb.Controllers
     {
         Registration_FormEntities db = new Registration_FormEntities();
 
-        public ActionResult Index() 
+        public ActionResult Index() //insert
         {
             List<State> state= db.States.ToList();
-            ViewBag.State1 = new SelectList(state, "State_ID", "State_Name"); //here problem ("as","as")
+            ViewBag.State1 = new SelectList(state, "State_Name", "State_Name"); 
 
             List<Country> Countr = db.Countries.ToList();
-            ViewBag.Country1 = new SelectList(Countr, "Country_ID", "Country_Name"); //here problem ("as","as")
+            ViewBag.Country1 = new SelectList(Countr, "Country_Name", "Country_Name");
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(Registration rs) //insert 
+        public ActionResult Index(Registration rs) //insert post 
         {
-            //iamge code
+            //imAge code
             string filename = Path.GetFileNameWithoutExtension(rs.ImageFile.FileName); //store filename without extension
             string exten=Path.GetExtension(rs.ImageFile.FileName);  //save extension
             filename = filename + exten;
@@ -35,12 +35,21 @@ namespace New_RegistrationForm24Feb.Controllers
             filename = Path.Combine(Server.MapPath("~/Images/"),filename); //teling server we have this path add filename
             rs.ImageFile.SaveAs(filename);
             db.Registrations.Add(rs);
-            int a=db.SaveChanges();
-            if (a > 0)
-                ViewBag.Message = "<script>alert('inserted')</script>";
-            else
-                ViewBag.Message = "<script>alert('not inserted')</script>";
+            //image code ends
 
+                int a = db.SaveChanges();
+
+                if (a > 0)
+                    ViewBag.Message = "<script>alert('inserted')</script>";
+                else
+                    ViewBag.Message = "<script>alert('not inserted')</script>";
+
+         
+            List<State> state = db.States.ToList();
+            ViewBag.State1 = new SelectList(state, "State_Name", "State_Name"); 
+
+            List<Country> Countr = db.Countries.ToList();
+            ViewBag.Country1 = new SelectList(Countr, "Country_Name", "Country_Name"); 
             return View();
         }
             
@@ -48,6 +57,22 @@ namespace New_RegistrationForm24Feb.Controllers
         {
             var data=db.Registrations.ToList();
             return View(data);
+        }
+
+        public ActionResult Delete(int id)  //returns the row u want to del
+        {
+            var RegIdRow = db.Registrations.Where(model=>model.ID==id).FirstOrDefault();
+            return View(RegIdRow);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Registration rs) //del the return row
+        {
+            db.Entry(rs).State =System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            TempData["Message"] = "<script>alert('Data_Deleted')</script>";
+            return RedirectToAction("Show");
+            //return View();
         }
 
     }
